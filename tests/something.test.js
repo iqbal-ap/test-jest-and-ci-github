16e4cf39d06ff5cec45af7b1a83463bb9b6782f0
iqbal-ap <iqbal.putra@bithealth.co.id>
2024-04-15 19:33:01 +0700

    feat: return to non-failed ci, remove ci for node v16.x, and test for full coverage

--- tests/something.test.js
+++ tests/something.test.js
@@ -1,17 +1,17 @@
 const { add, subtract, divide, multiply, power } = require('../something');
 
 describe('Something test', () => {
-    // test('Addition', () => {
-    //     expect(add(1, 2)).toBe(3);
-    // });
+    test('Addition', () => {
+        expect(add(1, 2)).toBe(3);
+    });
 
-    // test('Subtraction', () => {
-    //     expect(subtract(2, 1)).toBe(1);
-    // });
+    test('Subtraction', () => {
+        expect(subtract(2, 1)).toBe(1);
+    });
 
-    // test('Multiplication', () => {
-    //     expect(multiply(2, 2)).toBe(4);
-    // });
+    test('Multiplication', () => {
+        expect(multiply(2, 2)).toBe(4);
+    });
 
     test('Division', () => {
         expect(divide(4, 2)).toBe(2);
